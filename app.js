95855b13c302f12959d6e8b27ee02df22107b25b
mmelendez3 <mele0019@gmail.com>
2021-10-13 13:59:04 -0500

    update

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
+const fs = require('fs')
+const generatePage = require('./src/page-template.js')
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
+
+const [name, github] = profileDataArgs
 
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i])
-}
 
-profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
+
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//     console.log(profileDataArr[i])
+// }
+
+// profileDataArr.forEach((profileItem) => {
+//     console.log(profileItem)
+
+// })
+// }
+
+// printProfileData(profileDataArgs)
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio Complete! Check out index.html to see the output!')
+
 
 })
-}
-
-printProfileData(profileDataArgs)
-
-
-
