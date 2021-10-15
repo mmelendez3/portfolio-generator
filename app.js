3fcadbdcecd7585fe0fca60a732872a68c8406f2
mmelendez3 <mele0019@gmail.com>
2021-10-14 20:51:11 -0500

    added HTMl stuff

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-
 const inquirer = require('inquirer')
-// const fs = require('fs')
-// const generatePage = require('./src/page-template.js')
+const fs = require('fs')
+const generatePage = require('./src/page-template.js')
+
 
 // // const profileDataArgs = process.argv.slice(2, process.argv.length);
 
@@ -149,8 +149,69 @@ const promptUser = () => {
     })
   }
 
+  //Mock Data Erase when finished
+//   const mockData = 
+//     {
+//         name: 'Lernantino',
+//         github: 'lernantino',
+//         confirmAbout: true,
+//         about:
+//           'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+//         projects: [
+//           {
+//             name: 'Run Buddy',
+//             description:
+//               'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+//             languages: ['HTML', 'CSS'],
+//             link: 'https://github.com/lernantino/run-buddy',
+//             feature: true,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Taskinator',
+//             description:
+//               'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+//             languages: ['JavaScript', 'HTML', 'CSS'],
+//             link: 'https://github.com/lernantino/taskinator',
+//             feature: true,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Taskmaster Pro',
+//             description:
+//               'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+//             languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+//             link: 'https://github.com/lernantino/taskmaster-pro',
+//             feature: false,
+//             confirmAddProject: true
+//           },
+//           {
+//             name: 'Robot Gladiators',
+//             description:
+//               'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+//             languages: ['JavaScript'],
+//             link: 'https://github.com/lernantino/robot-gladiators',
+//             feature: false,
+//             confirmAddProject: false
+//           }
+//         ]
+//       };
+
+      
+      //Mock data ends here
+
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-      console.log(portfolioData)
+     const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+    
+    })
   })
+
+
+//only for mock data erase when done
+// const pageHTML = generatePage(mockData)
